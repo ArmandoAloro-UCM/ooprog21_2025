74e28bc20f93c4ab8bc5f25d24c223d9f0bf4317
ArmandoAloro-UCM <araraloro88@gmail.com>
2025-08-29 17:47:41 +0800

    Add files via upload

---
+++ Chapter_1/Comments.java
@@ -0,0 +1,23 @@
+public class Comments 
+{
+    public static void main(String[] args) 
+    {
+
+        // Single-line comment: simple greeting
+        System.out.println("Single-line comment syntax: begins with two forward slashes (//).");
+
+        /* Multi-line comment: explaining a block of code
+         * 
+         * 
+         * 
+         */
+        System.out.println("Multi-line comment syntax: begins with /* and ends with */.");
+
+        /**
+         * Documentation comment: describes the purpose of this print statement
+         * 
+         * 
+         */
+        System.out.println("Documentation comment syntax: begins with /** and ends with */.");       
+    }
+}
---
+++ Chapter_1/FavoriteSong.java
@@ -0,0 +1,10 @@
+public class FavoriteSong 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Saranggola'y lilipad sa kahel ng kalangitan");
+        System.out.println("Paalam na nga ba sa ating nakaraan");
+        System.out.println("Ngunit sa'n man tayo hipan ng amihan");
+        System.out.println("'Di ipagpapalit ang pagkakaibigan");
+    }
+}
