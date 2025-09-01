dbd936534f6f7f2b43518a6504bb6e234ce49ec1
ArmandoAloro-UCM <araraloro88@gmail.com>
2025-09-01 16:11:19 +0800

    Update FixDebugOne.java

--- Chapter_1/FixDebugOne.java
+++ Chapter_1/FixDebugOne.java
@@ -21,8 +21,9 @@ public class FixDebug One
 public class FixDebugOne 
 { 
     /* This program displays a greeting */ 
-    public static void main(String[] args) 
+    public static void main(String[]args) 
     { 
         System.out.println("Hello World!"); 
     } 
 }
+
