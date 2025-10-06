2e49b80026be7386fba8a9bdc4116bf392721e65
ArmandoAloro-UCM <araraloro88@gmail.com>
2025-10-06 18:07:52 +0800

    Update Payroll.java

--- Chapter_5/Payroll.java
+++ Chapter_5/Payroll.java
@@ -4,15 +4,13 @@ public class Payroll {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         
-        Employee worker = new Employee(1001, 25.0);
-        
         System.out.print("How many hours did you work this week ? ");
         double hoursWorked = input.nextDouble();
         
         System.out.print("What is your regular pay rate ? ");
         double payRate = input.nextDouble();
         
-        worker.setPayRate(payRate);
+        Employee worker = new Employee(1, payRate);
         
         double regularPay = worker.calculateRegularPay(hoursWorked);
         double overtimePay = worker.calculateOvertimePay(hoursWorked);
